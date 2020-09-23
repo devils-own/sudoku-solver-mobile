f857fe7eb5e8b4eef28f284b8008b95faff5ee3d
hritikaggarwal2 <hritikaggarwal2@gmail.com>
2020-09-23 04:50:57 -0700

    convert photo to grayscale

--- ios/OpenCV/RNOpenCvLibrary.mm
+++ ios/OpenCV/RNOpenCvLibrary.mm
@@ -14,4 +14,103 @@ RCT_EXPORT_METHOD(addEvent:(NSString *)name location:(NSString *)location)
   RCTLogInfo(@"Pretending to create an event %@ at %@", name, location);
 }
 
+RCT_EXPORT_METHOD(sudokuSolvedImage:(NSString *)imageAsBase64 callback:(RCTResponseSenderBlock)callback) {
+   UIImage* uiImage = [self decodeBase64ToImage:imageAsBase64];
+  
+   cv::Mat cvImg = [self convertUIImageToCVMat:uiImage];
+   cv::cvtColor(cvImg, cvImg, cv::COLOR_BGR2GRAY);
+  
+   UIImage* finalUiImage = [self converCVMatToUIImage:cvImg:uiImage.imageOrientation];
+    
+   NSString *finalImage = [self encodeToBase64String:finalUiImage];
+  
+  NSArray *dataArray = @[finalImage];
+  callback(@[[NSNull null], dataArray]);
+}
+
+- (UIImage *)decodeBase64ToImage:(NSString *)strEncodeData {
+  NSData *data = [[NSData alloc]initWithBase64EncodedString:strEncodeData options:NSDataBase64DecodingIgnoreUnknownCharacters];
+  return [UIImage imageWithData:data];
+}
+
+- (NSString *)encodeToBase64String:(UIImage *)image {
+ return [UIImageJPEGRepresentation(image, .5) base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
+}
+
+- (cv::Mat)convertUIImageToCVMat:(UIImage *)image {
+  CGColorSpaceRef colorSpace = CGImageGetColorSpace(image.CGImage);
+  
+  CGFloat cols;
+  CGFloat rows;
+  
+  if (image.imageOrientation == UIImageOrientationLeft || image.imageOrientation == UIImageOrientationRight) {
+    cols = image.size.height;
+    rows = image.size.width;
+  } else {
+    cols = image.size.width;
+    rows = image.size.height;
+  }
+  
+  cv::Mat cvMat(rows, cols, CV_8UC4); // 8 bits per component, 4 channels (color channels + alpha)
+  
+  CGContextRef contextRef = CGBitmapContextCreate(cvMat.data,                 // Pointer to  data
+                                                  cols,                       // Width of bitmap
+                                                  rows,                       // Height of bitmap
+                                                  8,                          // Bits per component
+                                                  cvMat.step[0],              // Bytes per row
+                                                  colorSpace,                 // Colorspace
+                                                  kCGImageAlphaNoneSkipLast |
+                                                  kCGBitmapByteOrderDefault); // Bitmap info flags
+  
+  CGContextDrawImage(contextRef, CGRectMake(0, 0, cols, rows), image.CGImage);
+  CGContextRelease(contextRef);
+  
+  return cvMat;
+}
+
+-(UIImage *)converCVMatToUIImage:(cv::Mat)cvMat:(UIImageOrientation) imageOrientation {
+    NSData *data = [NSData dataWithBytes:cvMat.data length:cvMat.elemSize()*cvMat.total()];
+
+    CGColorSpaceRef colorSpace;
+    CGBitmapInfo bitmapInfo;
+
+    if (cvMat.elemSize() == 1) {
+        colorSpace = CGColorSpaceCreateDeviceGray();
+        bitmapInfo = kCGImageAlphaNone | kCGBitmapByteOrderDefault;
+    } else {
+        colorSpace = CGColorSpaceCreateDeviceRGB();
+        bitmapInfo = kCGBitmapByteOrder32Little | (
+            cvMat.elemSize() == 3? kCGImageAlphaNone : kCGImageAlphaNoneSkipFirst
+        );
+    }
+
+    CGDataProviderRef provider = CGDataProviderCreateWithCFData((__bridge CFDataRef)data);
+
+    // Creating CGImage from cv::Mat
+    CGImageRef imageRef = CGImageCreate(
+        cvMat.cols,                 //width
+        cvMat.rows,                 //height
+        8,                          //bits per component
+        8 * cvMat.elemSize(),       //bits per pixel
+        cvMat.step[0],              //bytesPerRow
+        colorSpace,                 //colorspace
+        bitmapInfo,                 // bitmap info
+        provider,                   //CGDataProviderRef
+        NULL,                       //decode
+        false,                      //should interpolate
+        kCGRenderingIntentDefault   //intent
+    );
+
+    // Getting UIImage from CGImage
+    UIImage *finalImage = [UIImage imageWithCGImage:imageRef
+                                              scale:1
+                                        orientation:imageOrientation];
+    CGImageRelease(imageRef);
+    CGDataProviderRelease(provider);
+    CGColorSpaceRelease(colorSpace);
+
+    return finalImage;
+}
+
+
 @end
--- src/components/Camera.js
+++ src/components/Camera.js
@@ -15,15 +15,48 @@ const Camera = (props) => {
   const [toastRef, setToastRef] = useState();
   const [camera, setCamera] = useState(null);
 
+  const getPromiseForSudokuSolver = (imageAsBase64) => {
+    return new Promise((resolve, reject) => {
+      if (Platform.OS === 'android') {
+        OpenCV.sudokuSolvedImage(
+          imageAsBase64,
+          (error) => {
+            // error handling
+          },
+          (msg) => {
+            resolve(msg);
+          },
+        );
+      } else {
+        OpenCV.sudokuSolvedImage(imageAsBase64, (error, dataArray) => {
+          console.log(dataArray[0]);
+          resolve(dataArray[0]);
+        });
+      }
+    });
+  };
+
+  const getSolvedSudoku = (imageData) => {
+    getPromiseForSudokuSolver(imageData.base64)
+      .then((solvedPhoto) => {
+        setContent(solvedPhoto);
+        setIsPhotoPreview(true);
+        setPhotoPath(imageData.uri);
+      })
+      .catch((err) => {
+        console.log('err', err);
+      });
+  };
+
   const takePicture = async () => {
     // Take Photo and save it.
     if (camera != null) {
       const options = {quality: 0.5, base64: true};
       const data = await camera.takePictureAsync(options);
 
-      setContent(data.base64);
-      setIsPhotoPreview(true);
-      setPhotoPath(data.uri);
+      setIsPhotoPreview(false);
+
+      getSolvedSudoku(data);
 
       toastRef.show('Clicked!', 1000);
     }
